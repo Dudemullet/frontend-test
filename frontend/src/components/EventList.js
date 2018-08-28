3cd0a769f2fd457a47f235ab05e2f897d35966bd
Pipe Gutierrez <luis.gutierrez@konfio.mx>
2018-08-27 21:54:46 -0700

    Adds single component element

--- frontend/src/components/EventList.js
+++ frontend/src/components/EventList.js
@@ -1,26 +1,13 @@
 import React from 'react';
+import EventObj from './EventObj';
 
 const EventList = (props, context) => {
   const { events } = props;
 
   return <ul>
-    {events.map(event => {
-
-      // Prop destructuring
-      const { id,
-        title,
-        eventImage: image,
-        description,
-        location,
-        dates } = event;
-
+    {events.map( (event, i) => {
       return <li>
-        <div>
-          <header>
-            <a href="#{id}">{title}</a>
-          </header>
-          <p>{description}</p>
-        </div>
+        <EventObj event={event} id={i} />
       </li>
     })}
   </ul>
