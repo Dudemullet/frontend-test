b9bbbf88108049f3f097f59838ea3a912f2a1dba
Pipe Gutierrez <luis.gutierrez@konfio.mx>
2018-08-31 01:26:50 -0700

    Adds basic highlits style

--- frontend/src/components/Events/Highlights/HighlightsList.js
+++ frontend/src/components/Events/Highlights/HighlightsList.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import HighlightListItem from './HighlightListItem';
+import Header from '../../Header';
+
+import './HighlightsList.css';
 
 const HighlightsList = (props, context) => {
   const { events } = props;
 
-  return <ul>
-    {events.map( (event, i) => {
-      return <li key={i}>
-        <HighlightListItem event={event} />
-      </li>
-    })}
-  </ul>
+  return <React.Fragment>
+    <Header label="Highlights" />
+    <ul className="highlights-list-container">
+      {events.map( (event, i) => {
+        return <li key={i}>
+          <HighlightListItem event={event} />
+        </li>
+      })}
+    </ul>
+  </React.Fragment>
 }
 
 export default HighlightsList;
